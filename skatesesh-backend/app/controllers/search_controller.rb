6dc39f6272d5d2da6c68ee1f623e2b7b867c433a
Vicente <you@example.com>
2019-03-29 01:10:22 -0400

    successful request from backend

--- skatesesh-backend/app/controllers/search_controller.rb
+++ skatesesh-backend/app/controllers/search_controller.rb
@@ -2,9 +2,8 @@ class SearchController < ApplicationController
 skip_before_action :authorized, only: [:create]
 
     def create
-      # response = RestClient.get(`https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=#{params["lat"]},#{params["lng"]}&radius=2500&type=skatepark&keyword=skatepark&key=`)
+      response = RestClient.get "https://maps.googleapis.com/maps/api/place/nearbysearch/json?name=skatepark&location=#{params["lat"]},#{params["lng"]}&radius=2500&type=skatepark&keyword=skatepark&key=#{ENV['GOOGLE_KEY']}"
       byebug
-
     end
 
 
