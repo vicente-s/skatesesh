222b5dcbc4934ec3165e31c9314b66f06b5dadde
Vicente <you@example.com>
2019-03-29 00:21:10 -0400

    able to send location as params

--- skatesesh-backend/app/controllers/search_controller.rb
+++ skatesesh-backend/app/controllers/search_controller.rb
@@ -2,7 +2,7 @@ class SearchController < ApplicationController
 skip_before_action :authorized, only: [:create]
 
     def create
-      response = RestClient.get('https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=#{params["lat"]},#{params["lng"]}&radius=2500&type=skatepark&keyword=skatepark&key=')
+      # response = RestClient.get(`https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=#{params["lat"]},#{params["lng"]}&radius=2500&type=skatepark&keyword=skatepark&key=`)
       byebug
 
     end
