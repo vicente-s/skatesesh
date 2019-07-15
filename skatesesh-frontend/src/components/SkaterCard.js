c11e4d7f677dc3eaf4b3b8c1184a611a810f643f
Vicente <vicente.soriano@icloud.com>
2019-07-15 13:40:15 -0400

    team and skater images adjusted

--- skatesesh-frontend/src/components/SkaterCard.js
+++ skatesesh-frontend/src/components/SkaterCard.js
@@ -5,7 +5,7 @@ export default class SkaterCard extends Component {
     console.log(this.props)
     return (
           <div className="card col-2 mb-3 ml-4">
-            <img src={this.props.skater.img} class="card-img-top" alt="..."/>
+            <img src={this.props.skater.img} className="card-img-top" alt="..."/>
             <div className="card-body">
               <h5 className="card-title">{this.props.skater.first_name} {this.props.skater.last_name}</h5>
               <a href="" className="btn btn-info"> More Info </a>
--- skatesesh-frontend/src/components/TeamCard.js
+++ skatesesh-frontend/src/components/TeamCard.js
@@ -5,7 +5,7 @@ export default class TeamCard extends Component {
     return (
         <div className="col-4">
           <div className="card bg-dark text-white mb-3">
-            <img src={this.props.team.img} class="card-img" alt="..."/>
+            <img src={this.props.team.img} className="card-img" alt="..."/>
             <h5 className="card-title">{this.props.team.name}</h5>
           </div>
         </div>
