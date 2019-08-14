79cb5d7d37e8527cdd0d543993d1b77a69518cfb
Vicente <vicente.soriano@icloud.com>
2019-08-14 16:38:00 -0400

    package / package-lock.json modified

--- skatesesh-backend/app/controllers/skaters_controller.rb
+++ skatesesh-backend/app/controllers/skaters_controller.rb
@@ -1,5 +1,5 @@
 class SkatersController < ApplicationController
-  skip_before_action :authorized, only: [:index]
+  skip_before_action :authorized, only: [:index, :show]
 
   def index
     @skaters = Skater.all
--- skatesesh-frontend/src/index.js
+++ skatesesh-frontend/src/index.js
@@ -4,11 +4,13 @@ import {BrowserRouter} from 'react-router-dom'
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { Provider } from 'react-redux'
+
 
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>, document.getElementById('root'));
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
