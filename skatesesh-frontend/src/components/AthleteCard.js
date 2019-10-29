3375aab3b9c68872d0405fac7310b1debe5b07b3
Vicente <vicente.soriano@icloud.com>
2019-10-29 16:03:25 -0400

    started making athletes responsive

--- skatesesh-frontend/src/components/AthleteCard.js
+++ skatesesh-frontend/src/components/AthleteCard.js
@@ -4,13 +4,10 @@ import { Link } from 'react-router-dom'
 export default class AthleteCard extends Component {
   render() {
     return (
-          <div className="athlete-card card col-2 mb-3 ml-4">
-            <img src={this.props.athlete.img} className="card-img-top" alt="..."/>
-            <hr/>
-            {this.props.athlete.first_name} {this.props.athlete.last_name}
-            <hr/>
-            <Link to={`athletes/${this.props.athlete.id}`} className="btn btn-info" onClick={() => console.log("Sddd")}> More info </Link>
-          </div>
+      <div className="athlete-card col-4">
+        <img src={this.props.athlete.img} className="card-img" alt={this.props.athlete.name}/>
+        <p>{this.props.athlete.name}</p>
+      </div>
     )
   }
 }
--- skatesesh-frontend/src/components/SponsorCard.js
+++ skatesesh-frontend/src/components/SponsorCard.js
@@ -6,12 +6,9 @@ import { Link, Route } from 'react-router-dom'
 export default class SponsorCard extends Component {
   render() {
     return (
-        <div className="col-4">
-          <div className="card bg-dark text-white mb-3">
-            <img src={this.props.sponsor.img} className="card-img" alt="..."/>
-            <Link to={`/sponsors/${this.props.sponsor.id}`} className="btn btn-info more-info-button"> More info </Link>
+          <div className="sponsor-card">
+            <img src={this.props.sponsor.img} className="card-img" alt={this.props.sponsor.name}/>
           </div>
-        </div>
     )
   }
 }
